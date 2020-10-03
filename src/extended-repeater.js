954ee4ff28d58313122946bf4faca65494fa582a
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2020-10-03 10:42:47 +0300

    feat: create extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator,
+  } = options;
+
+  let newStr = String(str);
+  let newAddition = "";
+
+  if (String(addition) && addition !== undefined) {
+    newAddition = String(addition);
+    if (additionRepeatTimes) {
+      let newAdditionSeparator = "|";
+      if (additionSeparator) {
+        newAdditionSeparator = additionSeparator;
+      }
+      newAddition += newAdditionSeparator;
+      newAddition = newAddition.repeat(additionRepeatTimes);
+      newAddition = newAddition.slice(
+        0,
+        newAddition.length - newAdditionSeparator.length
+      );
+    }
+  }
+
+  if (repeatTimes) {
+    let newSeparator = "+";
+    if (separator) {
+      newSeparator = separator;
+    }
+    newStr = newStr + newAddition + newSeparator;
+    newStr = newStr.repeat(repeatTimes);
+    newStr = newStr.slice(0, newStr.length - newSeparator.length);
+  } else {
+    newStr += newAddition;
+  }
+
+  return newStr;
 };
   
