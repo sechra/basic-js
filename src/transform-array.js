37289a933636dd87b7248367478f540fd389d965
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2020-10-03 10:18:04 +0300

    feat: add work function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+const sequences = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev',
+  ];
+  if (!(arr instanceof Array)) throw new Error('Error');
+  
+  const result = [];
+  let isDiscarded = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (isDiscarded) {
+      isDiscarded = false;
+    } else if (arr[i] === '--discard-next') {
+      isDiscarded = true;
+    } else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next') {
+      result.pop();
+    } else if (arr[i] === '--double-next') {
+      if (arr[i + 1] !== undefined) result.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next') {
+      if (arr[i - 1] !== undefined) result.push(arr[i - 1]);
+    } else {
+      
+      if (!sequences.includes(arr[i])) result.push(arr[i]);
+    }
+  }
+  return result;
 };
